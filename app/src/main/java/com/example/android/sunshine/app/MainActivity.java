049fdbc9c6c20df49f2bc855baf90000afbe88e2
Itachi Uchiha <itachiuchiha@192.168.1.7>
2016-02-24 18:33:35 -0800

    Fill list adapter with fake data

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +57,10 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+
+        ArrayAdapter<String> weatherAdapter;        //weather adapter for weather forecast
+        ListView listView;
+
         public PlaceholderFragment() {
         }
 
@@ -57,6 +68,54 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //create array of "fake" data
+
+
+            ArrayList<String> weatherList = new ArrayList<String>();
+            weatherList.add("Today - Sunny - 88/63");
+            weatherList.add("Tomorrow - Foggy - 70/46");
+            weatherList.add("Weds - Cloudy - 72/63");
+            weatherList.add("Thurs - Rainy - 64/51");
+            weatherList.add("Fri - Foggy - 70/46");
+            weatherList.add("Sat - Sunny - 76/68");
+            weatherList.add("Sun - Sunny - 82/63");
+
+            /*code in tutorial in case my method does not work
+
+            String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
+                "Sun - Sunny - 80/68",
+            };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+             */
+
+            //The ArrayAdapter will take data from a source and use it to populate the ListView it is
+            //attached to.
+            weatherAdapter = new ArrayAdapter<String>(
+                    //the current content(this fragment's parent activity)
+                    getActivity(),
+                    //ID of list item layout
+                    R.layout.list_item_forecast,
+                    //ID of textview to populate
+                    R.id.list_item_forecast_textview,
+                    //weather forecast data
+                    weatherList);
+
+            //listView = (ListView) this.findViewById(R.id.listview_forecast);
+            //above does not work because we are not currently in a View object but a fragment object
+            //so to get appropriate view we must get the root view from the fragment layout
+            listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(weatherAdapter);
+
+
             return rootView;
         }
     }
