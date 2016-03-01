28d37a54330e124d9b46e91b7db7f11456677296
Itachi Uchiha <itachiuchiha@192.168.1.7>
2016-03-01 14:28:03 -0800

    Fix networking error by moving network operations to background threat.

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,157 @@
+package com.example.android.sunshine.app;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by itachiuchiha on 3/1/16.
+ */
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends android.support.v4.app.Fragment {
+
+
+    ArrayAdapter<String> weatherAdapter;        //weather adapter for weather forecast
+    ListView listView;
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //create array of "fake" data
+
+
+        ArrayList<String> weatherList = new ArrayList<String>();
+        weatherList.add("Today - Sunny - 88/63");
+        weatherList.add("Tomorrow - Foggy - 70/46");
+        weatherList.add("Weds - Cloudy - 72/63");
+        weatherList.add("Thurs - Rainy - 64/51");
+        weatherList.add("Fri - Foggy - 70/46");
+        weatherList.add("Sat - Sunny - 76/68");
+        weatherList.add("Sun - Sunny - 82/63");
+
+            /*code in tutorial in case my method does not work
+
+            String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
+                "Sun - Sunny - 80/68",
+            };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+             */
+
+        //The ArrayAdapter will take data from a source and use it to populate the ListView it is
+        //attached to.
+        weatherAdapter = new ArrayAdapter<String>(
+                //the current content(this fragment's parent activity)
+                getActivity(),
+                //ID of list item layout
+                R.layout.list_item_forecast,
+                //ID of textview to populate
+                R.id.list_item_forecast_textview,
+                //weather forecast data
+                weatherList);
+
+        //listView = (ListView) this.findViewById(R.id.listview_forecast);
+        //above does not work because we are not currently in a View object but a fragment object
+        //so to get appropriate view we must get the root view from the fragment layout
+        listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(weatherAdapter);
+        return rootView;
+    }
+
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
