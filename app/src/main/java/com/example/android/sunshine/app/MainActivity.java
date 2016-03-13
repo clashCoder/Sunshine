19bbb7aa5a976dfd7a3ae1876c7933e1c28a84e1
Luis Guillen <clashCoder@users.noreply.github.com>
2016-03-12 17:33:01 -0800

    Add Map menu option

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
-
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
 
     @Override
@@ -44,11 +48,53 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if (id == R.id.action_map) {
+            //TODO: Intent to view preferred Location
+
+            getPreferredLocationInMap();
+            /*SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+            String zipCode = sharedPreferences.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                    .appendQueryParameter("q", zipCode)
+                    .build();
+            intent.setData(geoLocation);
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+            else {
+                Log.d(LOG_TAG, "No apps installed to view" + zipCode);
+            }*/
+
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
+    private void getPreferredLocationInMap() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String zipCode = sharedPreferences.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", zipCode)
+                .build();
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d(LOG_TAG, "No apps installed to view" + zipCode);
+        }
+
+    }
+
 }
 
